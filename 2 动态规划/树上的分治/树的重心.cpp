2f94d4cf09c0cb9b00a025c829c2b82ad2b49eab
Strive-for-excellence <1505656319@qq.com>
2018-09-27 20:41:20 +0800

    Update 树的重心.cpp

--- 2 动态规划/树上的分治/树的重心.cpp
+++ 2 动态规划/树上的分治/树的重心.cpp
@@ -1,20 +1,23 @@
+// Size[u] 代表以节点u为根的子树节点个数
+// dp[u] 代表去除u节点后最大子树的节点个数
 const int maxn = 2e4+100;
 vector<int> G[maxn];
 int dp[maxn];
+int Size[maxn];
 int n;
-int dfs(int node,int fa){
-	int Max = 0;
-	dp[node] = 0;
-	int sum = 0;
-	for(int i = 0;i < G[node].size(); ++i){
-		if(fa==G[node][i])continue;
-		int tmp = dfs(G[node][i],node);
-		sum += tmp;
-		Max = max(Max,tmp);
+int ans;
+void  dfs(int u,int fa){
+	dp[u] = Size[u] = 0;
+	for(int i = 0;i < G[u].size(); ++i){
+		if(fa==G[u][i])continue;
+		dfs(G[u][i],u);
+		// sum += tmp;
+		Size[u] += Size[G[u][i]];
+		dp[u] = max(dp[u],Size[G[u][i]]);
 	}
-	sum++;
-	dp[node] = max(n-sum,Max);
-	return sum;
+	Size[u]++;
+	dp[u] = max(n-Size[u],dp[u]);
+	if(dp[u] < dp[ans]) ans = u;
 }
 int main(void)
 {
@@ -29,21 +32,10 @@ int main(void)
 			G[u].push_back(v);
 			G[v].push_back(u);
 		}
-
+		ans = 0;
+		dp[0] = INF;
 		dfs(1,-1);
-		int Min = n;
-		int ans;
-		// for(int i = 1;i <= n; ++i) dp[i] = max(n-dp[i],dp[i]);
-		for(int i = 1;i <= n; ++i) 
-			if(dp[i] < Min) {
-				ans = i;
-				Min = dp[i];
-		}
 		printf("%d %d\n",ans,dp[ans]);
 	}
-   
-
    return 0;
 }
-
-/*

8989b467c6e797f3d464461e82ac13c2cdfbd11d
Strive-for-excellence <1505656319@qq.com>
2018-11-01 22:38:32 +0800

    Create 1 单调队列.cpp

---
+++ 2 动态规划/1 单调队列.cpp
@@ -0,0 +1,48 @@
+//https://ac.nowcoder.com/acm/contest/223/C
+
+const int maxn = 1e5+100;
+int a[maxn];
+int s[maxn];// 单调栈
+// 第一遍求在这个区间里面最大
+int pre[maxn];
+int nxt[maxn];
+int main(void)
+{
+    int T,n;
+    cin>>T;
+    while(T--){
+        scanf("%d",&n);
+        for(int i = 1;i <= n; ++i){
+            scanf("%d",&a[i]);
+        }
+        int t = 0;
+        for(int i = 1;i <= n; ++i){
+            pre[i] = nxt[i] = 0;
+            while(t > 0&&a[i] > a[s[t]]) nxt[s[t]] = i,t--;
+            pre[i] = s[t];
+            s[++t] = i;
+            // cout<<pre[i]<<" ";
+        }
+        while(t > 0)
+            nxt[s[t]] = n+1,t--;
+        LL ans = 0;
+        for(int i = 1;i <= n; ++i){
+            ans += 1ll*a[i]*(nxt[i]-i)*(i-pre[i]);
+        }
+        t = 0;
+        for(int i = 1;i <= n; ++i){
+            pre[i] = nxt[i] = 0;
+            while(t > 0&&a[i] < a[s[t]]) nxt[s[t]] = i,t--;
+            pre[i] = s[t];
+            s[++t] = i;
+        }
+         while(t > 0)
+            nxt[s[t]] = n+1,t--;
+        for(int i = 1;i <= n; ++i){
+            ans -= 1ll*a[i]*(nxt[i]-i)*(i-pre[i]);
+        }
+        printf("%lld\n",ans);
+    }
+
+    return 0;
+}

14da8745d88b115ca4bd42ea12e2ca19df42e7ff
Strive-for-excellence <1505656319@qq.com>
2019-07-17 19:14:00 +0800

    Update 1 匈牙利算法.cpp

--- 3 图论/二分图/1 匈牙利算法.cpp
+++ 3 图论/二分图/1 匈牙利算法.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 #define mem(ar,num) memset(ar,num,sizeof(ar))
 #define me(ar) memset(ar,0,sizeof(ar))
@@ -13,26 +12,26 @@ typedef long long LL;
 typedef unsigned long long ULL;
 const int    prime = 999983;
 const int    INF = 0x7FFFFFFF;
-const LL     INFF =0x7FFFFFFFFFFFFFFF;
+const LL     INFF = 0x7FFFFFFFFFFFFFFF;
 const double pi = acos(-1.0);
 const double inf = 1e18;
 const double eps = 1e-6;
 const LL     mod = 1e9 + 7;
-LL qpow(LL a,LL b){LL s=1;while(b>0){if(b&1)s=s*a%mod;a=a*a%mod;b>>=1;}return s;}
-LL gcd(LL a,LL b) {return b?gcd(b,a%b):a;}
-int dr[2][4] = {1,-1,0,0,0,0,-1,1};
-typedef pair<int,int> P;
-const int maxn = 1000+10;
+LL qpow(LL a, LL b) {LL s = 1; while (b > 0) {if (b & 1)s = s * a % mod; a = a * a % mod; b >>= 1;} return s;}
+LL gcd(LL a, LL b) {return b ? gcd(b, a % b) : a;}
+int dr[2][4] = {1, -1, 0, 0, 0, 0, -1, 1};
+typedef pair<int, int> P;
+const int maxn = 1000 + 10;
 vector<int> G[maxn];
 int match[maxn];
 bool used[maxn];
-int N,M;
-bool dfs(int v){
+int N, M;
+bool dfs(int v) {
 	used[v] = true;
-	for(int i = 0;i < G[v].size(); ++i){
-		if(used[u]) continue; used[u] = true;
-		int u = G[v][i],w = match[u];
-		if(w < 0||!used[w]&&dfs(w)){
+	for (int i = 0; i < G[v].size(); ++i) {
+		// if (used[u]) continue; used[u] = true;
+		int u = G[v][i], w = match[u];
+		if (w < 0 || !used[w] && dfs(w)) {
 			match[v] = u;
 			match[u] = v;
 			return true;
@@ -42,25 +41,24 @@ bool dfs(int v){
 }
 int main(void)
 {
-    scanf("%d %d",&N,&M);
-    
-    while(M--){
-    	int u,v;
-    	scanf("%d %d",&u,&v);
-    	G[u].Pb(v);
-    	G[v].Pb(u);
+	scanf("%d %d", &N, &M);
+
+	while (M--) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		G[u].Pb(v);
+		G[v].Pb(u);
 	}
 	int ans = 0;
-	memset(match,-1,sizeof(match));
-	for(int i = 1;i <= N; ++i){
-		if(match[i] < 0){
-			memset(used,0,sizeof(used));
-			if(dfs(i)){
+	memset(match, -1, sizeof(match));
+	for (int i = 1; i <= N; ++i) {
+		if (match[i] < 0) {
+			memset(used, 0, sizeof(used));
+			if (dfs(i)) {
 				ans++;
 			}
 		}
 	}
-   cout<<ans<<endl;
-   return 0;
+	cout << ans << endl;
+	return 0;
 }
-

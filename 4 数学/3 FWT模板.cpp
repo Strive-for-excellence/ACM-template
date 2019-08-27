2179cee17b6ee3e5d5ddc606b32d158245f325cc
Strive-for-excellence <1505656319@qq.com>
2019-08-27 21:49:46 +0800

    Update 3 FWT模板.cpp

--- 4 数学/3 FWT模板.cpp
+++ 4 数学/3 FWT模板.cpp
@@ -1,25 +1,16 @@
-
-// 异或
-void FWT(int *a,int N,int opt){
-	const int inv2 = qpow(2,mod-2);
-	// j是区间开始点，i是区间距离，k是具体位置，j+k,i+j+k就是在a数组中的坐标
-	for(int i = 1;i < N; i <<= 1){
-		for(int p = i<<1,j = 0;j < N; j += p){
-			for(int k = 0;k < i; ++k){
-                int X = a[j+k],Y = a[i+j+k];
-                a[j+k] = (X+Y)%mod;
-                a[i+j+k] = (X+mod-Y)%mod;
-                if(opt == -1) a[j+k] = 1ll*a[j+k]*inv2%mod,a[i+j+k] = 1ll*a[i+j+k]*inv2%mod;
-
-
-			}
-		}
-	}
+inline void FWT(int *a,int n,int f){
+    for (int k=1;k<n;k<<=1)
+        for (int i=0;i<n;i+=(k<<1))
+            for (int j=0;j<k;j++)
+                if (f==1){
+                    int x=a[i+j],y=a[i+j+k];
+                    //and:a[i+j]+=a[i+j+k];
+                    //or :a[i+j+k]+=a[i+j];
+                    //xor:a[i+j]=x+y;a[i+j+k]=x-y;
+                } else{
+                    int x=a[i+j],y=a[i+j+k];
+                    //and:a[i+j]-=a[i+j+k];
+                    //or :a[i+j+k]-=a[i+j];
+                    //xor:a[i+j]=(x+y)/2;a[i+j+k]=(x-y)/2;
+                }
 }
-
- 或   
-if(opt == 1) F[i+j+k] = (F[i+j+k]+F[j+k]) %mod;
-else         F[i+j+k] = (F[i+j+k+mod-F[j+k]) %mod;
-和
-if(opt == 1) F[j+k] = (F[j+k]+F[i+j+k]) %mod;
-else         F[j+k] = (F[j+k] +mod-F[i+j+k])%mod;

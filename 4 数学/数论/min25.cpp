54f8277e7bb13bebfead7d63e5e1fc76edc3f549
Strive-for-excellence <1505656319@qq.com>
2019-09-26 01:56:49 +0800

    Update min25.cpp

--- 4 数学/数论/min25.cpp
+++ 4 数学/数论/min25.cpp
@@ -1,84 +1,71 @@
-
-#include <bits/stdc++.h>
-#define mem(ar,num) memset(ar,num,sizeof(ar))
-#define me(ar) memset(ar,0,sizeof(ar))
-#define lowbit(x) (x&(-x))
-#define Pb push_back
-#define  FI first
-#define  SE second
-#define rep(i,a,n) for (int i=a;i<n;i++)
-#define per(i,a,n) for (int i=n-1;i>=a;i--)
-#define IOS ios::sync_with_stdio(false)
-#define DEBUG cout<<endl<<"DEBUG"<<endl; 
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-typedef unsigned long long ULL;
-const int    prime = 999983;
-const int    INF = 0x7FFFFFFF;
-const LL     INFF =0x7FFFFFFFFFFFFFFF;
-const double pi = acos(-1.0);
-const double inf = 1e18;
-const double eps = 1e-6;
-const LL     mod = 1e9 + 7;
-LL qpow(LL a,LL b){LL s=1;while(b>0){if(b&1)s=s*a%mod;a=a*a%mod;b>>=1;}return s;}
-LL gcd(LL a,LL b) {return b?gcd(b,a%b):a;}
-int dr[2][4] = {1,-1,0,0,0,0,-1,1};
-LL n,sqr;
-const int maxn = 100100;
-int Prime[maxn],vis[maxn],tot,gp1[maxn],gp2[maxn];
-const int inv2 = qpow(2,mod-2),inv6 = qpow(6,mod-2);
-LL w[maxn*2],sw;
+const int mod = 1e9+7;
+LL qpow(LL a,LL b){LL s= 1;while(b>0){if(b&1)s = s*a%mod; a = a*a%mod,b >>=1;}return s;}
+
+const int maxn = 2e5+10;
+int Prime[maxn],tot;
+bool vis[maxn];
+int gp1[maxn],gp2[maxn];
 int g1[maxn*2],g2[maxn*2];
-int id1[maxn],id2[maxn];
-
-int S(LL x, int y) {
-	if (Prime[y] >= x) return 0;
-	int  p = x <= sqr ? id1[x] : id2[n / x];
-	int ret = ((0LL + g2[p] - g1[p] - (gp2[y] - gp1[y])) % mod + mod) % mod;
-	for (int i = y + 1; i <= tot && 1LL * Prime[i]*Prime[i] <= x; ++i) {
-		LL pe = Prime[i];
-		for (int e = 1; pe <= x; ++e, pe *= Prime[i]) {
-			int o = pe % mod;
-			ret = (ret + 1LL * o * (o - 1) % mod * (S(x / pe, i) + (e != 1))) % mod;
-		}
-	}
-	return ret;
-}
-
-int main(void){
-	scanf("%lld",&n);
-	sqr = sqrt(n);
-	vis[1] = 1;
-	for(int i = 2;i <= sqr; ++i){
-		if(!vis[i]) Prime[++tot] = i;
-		for(int j = 1;j <= tot&&i*Prime[j] <= sqr; ++j){
+LL val[maxn*2];
+int id1[maxn],id2[maxn],cnt;
+LL n,sqr;
+void init(int N){
+	for(int i = 2;i <= N; ++i){
+		if(!vis[i])Prime[++tot] = i;
+		for(int j = 1;j <= tot&&i*Prime[j]<=N; ++j){
 			vis[i*Prime[j]] = 1;
-			if(i%Prime[j] == 0) break;
+			if(i%Prime[j]==0) break;
 		}
 	}
-	for(int i = 1;i <= tot; ++i)
-		gp1[i] =(gp1[i-1]+Prime[i])%mod,gp2[i]=(gp2[i-1]+1ll*Prime[i]*Prime[i])%mod;
-	for(LL l = 1,r;l <= n; l = r+1){
-		r = n/(n/l);
-		w[++sw] = n/r;
-		g1[sw] = w[sw]%mod;
-		g2[sw] = 1ll*g1[sw]*(g1[sw]+1)%mod*(2*g1[sw]+1)%mod*inv6%mod-1;
-		g1[sw] = 1ll*g1[sw]*(g1[sw]+1)%mod*inv2%mod-1;
-		if(n/r <= sqr) id1[n/r] =sw;
-		else id2[r] = sw;
+	for(int i = 1;i <= tot; ++i){
+		gp1[i] = (gp1[i-1]+Prime[i])%mod;
+		gp2[i] = (gp2[i-1]+1ll*Prime[i]*Prime[i])%mod;
 	}
-	for(int i= 1;i <= tot; ++i){
-		LL tmp = 1ll*Prime[i]*Prime[i];
-		for(int j = 1;j <= sw&&tmp <= w[j];++j){
-			LL p = w[j]/Prime[i];
-			p = (p <= sqr?id1[p]:id2[n/p]);
-			g1[j] = (g1[j]-1ll*Prime[i]*(g1[p]-gp1[i-1])%mod+mod)%mod;
-			g2[j] = (g2[j]-1ll*Prime[i]*Prime[i]%mod*(g2[p]-gp2[i-1])%mod+mod)%mod;
+}
+const LL inv2 = qpow(2,mod-2),inv6 = qpow(6,mod-2);
+inline int S1(LL n){n%=mod;return n*(n+1)%mod*inv2%mod;}
+inline int S2(LL n){n%=mod;return n*(n+1)%mod*(2*n+1)%mod*inv6%mod;}
+inline int id(LL t){return t <= sqr?id1[t]:id2[n/t];}
 
+// sum(f[i])(i \in P)
+void Getp(){
+	for(LL i = 1,j;i <= n;i = j+1){
+		j = n/(n/i);
+		val[++cnt] = n/i;
+		g1[cnt] = S1(n/i)-1;
+		g2[cnt] = S2(n/i)-1;
+		if(val[cnt] <= sqr) id1[val[cnt]] = cnt;
+		else id2[n/val[cnt]]= cnt;
+	}
+	for(int i = 1;i <= tot; ++i){
+		LL t = 1ll*Prime[i]*Prime[i];
+		for(int j = 1;j <= cnt&&t <= val[j];++j){
+			int p = id(val[j]/Prime[i]);
+			g1[j] = (g1[j]-1ll*Prime[i]*(g1[p]-gp1[i-1]))%mod;
+			g2[j] = (g2[j]-1ll*Prime[i]*Prime[i]%mod*(g2[p]-gp2[i-1]))%mod;
 		}
-
 	}
-	printf("%lld\n",(S(n,0)%mod+mod+1)%mod);
-
+}
+int S(LL now,int t){
+	if(now <= Prime[t]) return 0;
+	int p = id(now);
+	LL res = (g2[p]-g1[p]-1ll*(gp2[t]-gp1[t]))%mod;
+	for(int i = t+1;i <= tot&&1ll*Prime[i]*Prime[i]<=now; ++i){
+		LL pe = Prime[i];
+		for(int j = 1;pe <= now;++j,pe *= Prime[i]){
+			int o = pe%mod;
+			res = (res+1ll*o*(o-1)%mod*(S(now/pe,i)+(j!=1)))%mod;
+		}
+	}
+	return res;
+}
+int main(void){
+	cin>>n;sqr = sqrt(n+0.5);
+	init(sqr);
+	Getp();
+	cout<<(S(n,0)+1+mod)%mod<<endl;
 	return 0;
 }

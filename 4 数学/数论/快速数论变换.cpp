76d01db3b05bbb823a39312e43fc3494bea953a2
Strive-for-excellence <1505656319@qq.com>
2019-10-25 15:56:56 +0800

    Update 快速数论变换.cpp

--- 4 数学/数论/快速数论变换.cpp
+++ 4 数学/数论/快速数论变换.cpp
@@ -1,17 +1,7 @@
 const int mod = 998244353;
 LL qpow(LL a,LL b){LL s=1;while(b>0){if(b&1)s=s*a%mod;a=a*a%mod;b>>=1;}return s;}
 const int g = 3;  //原根
-LL quick_mod(LL a,LL b)
-{
-    LL ans=1;
-    for(;b;b/=2)
-    {
-        if(b&1)
-            ans=ans*a%mod;
-        a=a*a%mod;
-    }
-    return ans;
-}
+
 int rev(int x,int r)  //蝴蝶操作
 {
     int ans=0;
@@ -41,7 +31,7 @@ void NTT(int n, LL  A[],int on) // 长度为N (2的次数)
     for(int s=1; s<=r; s++)
     {
         int m=1<<s;
-        LL wn=quick_mod(g,(mod-1)/m);
+        LL wn=qpow(g,(mod-1)/m);
         for(int k=0; k<n; k+=m)
         {
             LL  w=1;
@@ -60,7 +50,7 @@ void NTT(int n, LL  A[],int on) // 长度为N (2的次数)
     {
         for(int i=1;i<n/2;i++)
             swap(A[i],A[n-i]);
-        LL inv=quick_mod(n,mod-2);
+        LL inv=qpow(n,mod-2);
         for(int i=0;i<n;i++)
             A[i]=A[i]%mod*inv%mod;
     }
